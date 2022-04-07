879fe9139a62d4cca5e6673a8a0af9e129a6f89c
Christhofer Laurent Juliant <94282400+FrldyZ@users.noreply.github.com>
2022-04-07 23:17:01 +0700

    ilangin confirm pw

--- resources/views/auth-register.blade.php
+++ resources/views/auth-register.blade.php
@@ -43,12 +43,6 @@
                                 <i class="bi bi-shield-lock"></i>
                             </div>
                         </div>
-                        <div class="form-group position-relative has-icon-left mb-4">
-                            <input type="password" class="form-control form-control-xl" placeholder="Confirm Password">
-                            <div class="form-control-icon">
-                                <i class="bi bi-shield-lock"></i>
-                            </div>
-                        </div>
                         <button class="btn btn-warning btn-block btn-lg shadow-lg mt-5">Sign Up</button>
                     </form>
                     <div class="text-center mt-5 text-lg fs-4">
