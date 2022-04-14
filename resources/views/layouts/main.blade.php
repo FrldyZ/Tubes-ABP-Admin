3de02d3540a2259a2c195cffa6e4c4c21a5cf368
ImamRasy <95610500+ImamRasy@users.noreply.github.com>
2022-04-14 13:24:15 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -20,13 +20,13 @@
             <div class="sidebar-wrapper active">
                 <div class="sidebar-header">
                     <div class="d-flex justify-content-between">
-                        <div class="logo">
-                            <a href="/dashboard"><img src="assets/images/logo/logo.png" alt="Logo" srcset=""></a>
-                        </div>
                         <div class="toggler">
                             <a href="#" class="sidebar-hide d-xl-none d-block"><i class="bi bi-x bi-middle"></i></a>
                         </div>
                     </div>
+                </div>
+                <div class="logo">
+                    <a href="/dashboard"><img src="assets/images/logo/logo.png" alt="Logo" srcset="" width="100" height="100"></a>
                 </div>
                 <div class="sidebar-menu">
                     <ul class="menu">
