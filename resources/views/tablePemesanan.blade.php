144056fe8dd39f86fdf7c0f23e07cc2b96714582
Kevin <96371699+kdrm12@users.noreply.github.com>
2022-04-16 07:59:48 +0700

    Update tablePemesanan.blade.php

--- resources/views/tablePemesanan.blade.php
+++ resources/views/tablePemesanan.blade.php
@@ -5,91 +5,97 @@
 
 @section('content')
 <style>
-  .row {
-    display: -ms-flexbox;
-    display: flex;
-    -ms-flex-wrap: wrap;
-    flex-wrap: wrap;
-    margin: 0 -16px;
-  }
-  
-  .col-25 {
-    -ms-flex: 25%;
-    flex: 25%;
-    padding: 0 16px;
-  }
-  
-  .container {
-    background-color: #f2f2f2;
-    padding: 5px 20px 15px 20px;
-    border: 1px solid lightgrey;
-    border-radius: 3px;
-  }
-  .subcontainer {
-    background-color: #fc0000;
-    padding: 5px 20px 15px 20px;
-    border: 1px solid lightgrey;
-    border-radius: 3px;
-  }
-  span.price {
-    float: right;
-    color: grey;
-  }
-  
-  @media (max-width: 800px) {
     .row {
-      flex-direction: column-reverse;
+        display: -ms-flexbox;
+        display: flex;
+        -ms-flex-wrap: wrap;
+        flex-wrap: wrap;
+        margin: 0 -16px;
     }
+
     .col-25 {
-      margin-bottom: 20px;
+        -ms-flex: 25%;
+        flex: 25%;
+        padding: 0 16px;
     }
-  }
 
-  table, th, td {
-      border: 1px solid black;
-      border-collapse: separate;
-  }
-  table {
-    width: 100%;
+    .container {
+        background-color: #f2f2f2;
+        padding: 5px 20px 15px 20px;
+        border: 1px solid lightgrey;
+        border-radius: 3px;
+    }
 
-  }
+    .subcontainer {
+        background-color: #fc0000;
+        padding: 5px 20px 15px 20px;
+        border: 1px solid lightgrey;
+        border-radius: 3px;
+    }
 
-  table.center {
-    margin-left: auto; 
-    margin-right: auto;
-  }
-  </style>
-  
-  <body>
-  <div class="row">
-      <div class="col-25">
-        <div class="container">
-          <h4>Cart
-            <span class="price" style="color:black">
-              <i class="fa fa-shopping-cart"></i>
-              <b>4</b>
-            </span>
-          </h4>
-          <table class="center">
-            <thead>
-              <tr>
-                <th scope="col" style=" width: 50%;">ID Pemesanan</th>
-                <th scope="col" style=" width: 25%;">Nama Barang</th>
-                <th scope="col" style=" width: 25%;">Harga</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="text-bold-500">ID</td>
-                <td class="text-bold-500">nambar</td>
-                <td class="text-bold-500">harga</td>
-              </tr>
-            </tbody>
-          </table>
-          <hr>
-          <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
+    span.price {
+        float: right;
+        color: grey;
+    }
+
+    @media (max-width: 800px) {
+        .row {
+            flex-direction: column-reverse;
+        }
+
+        .col-25 {
+            margin-bottom: 20px;
+        }
+    }
+
+    table,
+    th,
+    td {
+        border: 1px solid black;
+        border-collapse: separate;
+    }
+
+    table {
+        width: 100%;
+
+    }
+
+    table.center {
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
+
+<body>
+    <div class="row">
+        <div class="col-25">
+            <div class="container">
+                <h4>Cart
+                    <span class="price" style="color:black">
+                        <i class="fa fa-shopping-cart"></i>
+                        <b>4</b>
+                    </span>
+                </h4>
+                <table class="center">
+                    <thead>
+                        <tr>
+                            <th scope="col" style=" width: 50%;">ID Pemesanan</th>
+                            <th scope="col" style=" width: 25%;">Nama Barang</th>
+                            <th scope="col" style=" width: 25%;">Harga</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td class="text-bold-500">ID</td>
+                            <td class="text-bold-500">nambar</td>
+                            <td class="text-bold-500">harga</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <hr>
+                <p>Total <span class="price" style="color:black"><b>$30</b></span></p>
+            </div>
         </div>
-      </div>
-  </div>
-  </body>
+    </div>
+</body>
 @endsection
